3c71f71c6980b493610c8fefabd2fb1845ec6617
Amy Franz <amy.franz@gmail.com>
2020-08-30 16:33:38 +0100

    fixed port

--- App.js
+++ App.js
@@ -31,4 +31,10 @@ app.use("/users", userRoute);
 
 //ROUTES
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
